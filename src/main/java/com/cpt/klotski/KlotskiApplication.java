417083b2af0aaac2700e5974600736aee0970792
chuckthemole <chuckthemole@gmail.com>
2020-12-09 18:59:03 -0800

    Nothing

--- src/main/java/com/cpt/klotski/KlotskiApplication.java
+++ src/main/java/com/cpt/klotski/KlotskiApplication.java
@@ -9,9 +9,7 @@ public class KlotskiApplication {
 	public static void main(String[] args) {
 		// This is how normal Spring Boot app would be launched
 		// SpringApplication.run(SpringBootExampleApplication.class, args);
-
-		// JavaFxApplication doesn't exist yet,
-		// we'll create it in the next step
+		
 		Application.launch(Klotski.class, args);
 	}
 }
