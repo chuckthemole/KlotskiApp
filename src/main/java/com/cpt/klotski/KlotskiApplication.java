8f957d881b26fd315c2c4b6ef00dc2239983a6b2
chuckthemole <chuckthemole@gmail.com>
2020-12-09 19:09:02 -0800

    Stuff

--- src/main/java/com/cpt/klotski/KlotskiApplication.java
+++ src/main/java/com/cpt/klotski/KlotskiApplication.java
@@ -9,7 +9,7 @@ public class KlotskiApplication {
 	public static void main(String[] args) {
 		// This is how normal Spring Boot app would be launched
 		// SpringApplication.run(SpringBootExampleApplication.class, args);
-		
+
 		Application.launch(Klotski.class, args);
 	}
 }
