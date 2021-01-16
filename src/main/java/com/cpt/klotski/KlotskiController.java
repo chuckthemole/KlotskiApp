380d5544eb171da34c0ef7670370337147538825
chuckthemole <chuckthemole@gmail.com>
2021-01-16 11:38:45 -0800

    Build

--- src/main/java/com/cpt/klotski/KlotskiController.java
+++ src/main/java/com/cpt/klotski/KlotskiController.java
@@ -2,8 +2,15 @@ package com.cpt.klotski;
 
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component
 @FxmlView("main-scene.fxml")
+@RestController
 public class KlotskiController {
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World";
+    }
 }
