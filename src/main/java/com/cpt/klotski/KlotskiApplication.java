ae6d3b3d26ca33ea7a75af2a1ab14fb855e7af0f
chuckthemole <chuckthemole@gmail.com>
2021-01-16 11:30:39 -0800

    Build

--- src/main/java/com/cpt/klotski/KlotskiApplication.java
+++ src/main/java/com/cpt/klotski/KlotskiApplication.java
@@ -3,13 +3,20 @@ package com.cpt.klotski;
 //import org.springframework.boot.SpringApplication;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KlotskiApplication {
 	public static void main(String[] args) {
 		// This is how normal Spring Boot app would be launched
 		// SpringApplication.run(SpringBootExampleApplication.class, args);
 
 		Application.launch(Klotski.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
 	}
 }
